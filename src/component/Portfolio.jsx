66ae2bb9745961e05c7809b1c884dfa5a604e812
fahmi0604 <fahmifirmansyah917@gmail.com>
2021-03-10 15:21:02 +0700

    change layout portfolio

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import './Portfolio.css'
@@ -11,13 +11,15 @@ const Portfolio = () => {
         Aos.init({ duration: 2000 });
     }, []);
 
-    return(
+    return (
         <div className="portfolio" id="Portfolio">
             <div id="title-1">PORTFOLIO</div>
             <div id="title-2">Checkout a few of my works</div>
-            <div className="portfolio-body">
-                <div className="portfolio-1">
-                    <img data-aos="slide-up" src={myMushaf} alt="quran"/>
+            <div className="row-1">
+                <div className="column-port-1" id="port-img-1">
+                    <img data-aos="slide-up" src={myMushaf} alt="quran" />
+                </div>
+                <div className="column-port-1" id="port-text-1">
                     <div id="title-body-1">APPLICATION</div>
                     <div id="project-title-1">My Mushaf</div>
                     <div id="project-body-1">
@@ -26,22 +28,28 @@ const Portfolio = () => {
                         </div>
                     <div id="project-view-1">View Project</div>
                 </div>
-                <div className="portfolio-2">
-                    <img data-aos="slide-up" src={KronosWatch} alt="timer"/>
+            </div>
+            <div className="row-2">
+                <div className="column-port-2" id="port-img-2">
+                    <img data-aos="slide-up" src={KronosWatch} alt="timer" />
+                </div>
+                <div className="column-port-2" id="port-text-2">
                     <div id="title-body-2">APPLICATION</div>
                     <div id="project-title-2">Kronos watch</div>
                     <div id="project-body-2">
-                        {/* My Mushaf adalah aplikasi Quran pocket yang saya buat sendiri, dimana di dalam nya sudah terdapat alarm sholat, serta audio di setiap surat-surat al-quran */}
                         Chronos watch is a time management application that I made myself. where there is already a countdown timer feature, as well as a stopwatch with an elegant appearance
                         </div>
                     <div id="project-view-2">View Project</div>
                 </div>
-                <div className="portfolio-3">
-                    <img data-aos="slide-up" src={Apotik} alt="apotik"/>
+            </div>
+            <div className="row-3">
+                <div className="column-port-3" id="port-img-3">
+                    <img data-aos="slide-up" src={Apotik} alt="apotik" />
+                </div>
+                <div className="column-port-3" id="port-text-3">
                     <div id="title-body-3">WEB-BASED INFORMATION SYSTEM</div>
                     <div id="project-title-3">Pharmacy Information System</div>
                     <div id="project-body-3">
-                        {/* My Mushaf adalah aplikasi Quran pocket yang saya buat sendiri, dimana di dalam nya sudah terdapat alarm sholat, serta audio di setiap surat-surat al-quran */}
                         Pharmacy information system is a simple information system website about a pharmacy that I made myself, where there is already a login feature and is already based on the Mysql CRUD database.
                         </div>
                     <div id="project-view-3">View Project</div>
