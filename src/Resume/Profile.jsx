b993f397cc62e92d66f1d6f36d669ec46d62383c
fahmi0604 <fahmifirmansyah917@gmail.com>
2021-03-11 23:56:12 +0700

    add react-route and resume page

---
+++ src/Resume/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        <div className="Profile">
+            <div className="profile-content">
+                <div id="profile-title">Profile</div>
+                <div id="profile-title-2">I am a student majoring in Information Technology from the Malang State Polytechnic campus who has the ambition to create my own startup.</div>
+                <div id="profile-body"></div>
+                <div className="title-body">
+                    Name:<div id="profile-name">Fahmi Firmansyah</div>
+                </div>
+                <div className="title-body">
+                    Address:<div id="profile-address">Jl. Simpang tiga ex menangas Km 01 banyubiru Pandan, Pandan, Kebonrejo, Kec. Grati, Pasuruan, Jawa Timur 67184</div>
+                </div>
+                <div className="title-body">
+                    Phone: <div id="profile-phone">082118598937</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
